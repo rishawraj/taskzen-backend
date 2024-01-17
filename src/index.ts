43a35e495105efede5bf355983292448753a0f11
rishawraj <rishawraj0703@gmail.com>
2024-01-17 19:55:21 +0530

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
-// import taskRoutes from "./routes/taskRoutes.js";
 import taskRoutes from "./routes/taskRoutes";
 import listRoutes from "./routes/listRoutes";
 import tagRoutes from "./routes/tagRoutes";
 import authRoutes from "./routes/authRoutes";
+import compression from "compression";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 8000;
 mongoose.connect(process.env.MONGODB_URI || "");
 
 app.use(express.json());
+app.use(compression());
 
 app.use("/api", taskRoutes);
 app.use("/api", listRoutes);
