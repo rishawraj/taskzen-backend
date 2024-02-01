93695d29f3cb67d0e78de9a0f1313b00e5a018bc
rishawraj <rishawraj0703@gmail.com>
2024-02-02 02:35:17 +0530

    add ssl certificate & deploy to aws ec2 instance

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import tagRoutes from "./routes/tagRoutes";
 import authRoutes from "./routes/authRoutes";
 import compression from "compression";
 import cors from "cors";
+import fs from "fs";
+import https from "node:https";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -19,11 +21,20 @@ app.use(express.json());
 app.use(compression());
 app.use(cors());
 
+const options = {
+  key: fs.readFileSync("cert/key.pem"),
+  cert: fs.readFileSync("cert/cert.pem"),
+};
+
+console.log(options);
+
 app.use("/api", taskRoutes);
 app.use("/api", listRoutes);
 app.use("/api", tagRoutes);
 app.use("/api", authRoutes);
 
-app.listen(PORT, () => {
+const server = https.createServer(options, app);
+
+server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
